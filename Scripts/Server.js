ec48b11040282a4db627460b52cb52014fea38fd
mista_oliva <antoine.porignaux@gmail.com>
2024-12-19 10:16:03 +0100

    Server base code

---
+++ Scripts/Server.js
@@ -0,0 +1,10 @@
+import expressModule from 'express';
+import fsPromise from 'node:fs/promises';
+import pathLibrary from 'path';
+
+const app = expressModule();
+
+app.use(expressModule.static('source'));
+app.listen(3010, () => {
+  console.log('Server is running on port 3010');
+});
