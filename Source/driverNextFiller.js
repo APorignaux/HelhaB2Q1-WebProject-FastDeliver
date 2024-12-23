abad7a19e3baa621f1264248d9d9da327567b4f7
mista_oliva <antoine.porignaux@gmail.com>
2024-12-23 13:06:01 +0100

    driverNextfiller.js fetchCurrentDelivery()

---
+++ Source/driverNextFiller.js
@@ -0,0 +1,62 @@
+const currentDeliveryTemplate= document.body.querySelector('#currentDeliveryTemplate');
+const currentDeliverycore = document.body.querySelector('#current-delivery-core-info');
+const currentDeliveryDiv = document.body.querySelector('#currentDeliveryDiv');
+const packageNumber = document.body.querySelector('#packageNumber');
+
+const nextDeliveryTemplate = document.body.querySelector('#nextDeliveryTemplate');
+const nextDeliveryDiv = document.body.querySelector('#delivery-core-info');
+
+const livreurMail = 'emma@fastdeliver.com';
+
+async function fetchCurrentDelivery() {
+    currentDeliverycore.innerHTML = '';
+    const response = await fetch('/Livraisons');
+    const DeliveryData = await response.json();
+    const delivery = DeliveryData.results;
+    let driverDeliveries = [];
+
+    const responseClient = await fetch('/Client');
+    const ClientData = await responseClient.json();
+    const client = ClientData.results;
+    let currentClient = undefined;
+
+    //chercher dans les livraisons celle qui appartient au livreur connecté et la plus recente
+    Object.values(delivery).forEach(deliveryItem => {       //Object.value() fais une liste avec un JSON Output: ['John', 30, 'New York']
+        if (deliveryItem.Livreur === livreurMail) {         //deliveryItem représente une paire clé-valeur ex,  Livreur: 'emma@fastdeliver.com'
+            driverDeliveries.push(deliveryItem);
+        }
+    });
+    // Sort deliveries by date from earliest to latest
+    driverDeliveries.sort((a, b) => new Date(a.Date) - new Date(b.Date));
+
+    // fill current delivery
+    const currentDelivery = driverDeliveries[0];
+    const currentDeliveryRow = currentDeliveryTemplate.content.cloneNode(true);
+
+    //find the right client
+    Object.values(client).forEach(clientItem => {
+        if(clientItem.Email === currentDelivery.ClientEmail){
+            currentClient = clientItem;
+        }
+    });
+
+    const currentDeliveryTrackId = currentDeliveryDiv.querySelector('#packageNumber');
+    currentDeliveryTrackId.innerHTML = currentDelivery.NumSuivis;
+
+    const currentDeliveryName = currentDeliveryRow.querySelector('.current-delivery-name');
+    currentDeliveryName.innerHTML = currentClient.Nom;    //table client
+
+    const currentDeliveryphone = currentDeliveryRow.querySelector('.current-delivery-phone');
+    currentDeliveryphone.innerHTML = currentClient.Telephone;   //table client
+
+    const currentDeliveryAddress = currentDeliveryRow.querySelector('.current-delivery-address');
+    currentDeliveryAddress.innerHTML = currentDelivery.Addresse;
+
+    const currentDeliveryInstructions = currentDeliveryRow.querySelector('.current-delivery-instructions');
+    currentDeliveryInstructions.innerHTML = currentDelivery.Instructions;
+
+    currentDeliverycore.appendChild(currentDeliveryRow);
+}
+
+window.onload = fetchCurrentDelivery;
+
